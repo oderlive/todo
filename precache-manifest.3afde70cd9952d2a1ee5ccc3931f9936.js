ed5582f668852a45978699d100631803fde20386
oderlive <art20012004@gmail.com>
2022-10-30 23:51:47 +0300

    Updates

---
+++ precache-manifest.3afde70cd9952d2a1ee5ccc3931f9936.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a6281bd2f937fe7745e",
+    "url": "/oderlive/todo.git/static/css/main.dd79ee31.chunk.css"
+  },
+  {
+    "revision": "8a6281bd2f937fe7745e",
+    "url": "/oderlive/todo.git/static/js/main.8a6281bd.chunk.js"
+  },
+  {
+    "revision": "b26be9da27985059de63",
+    "url": "/oderlive/todo.git/static/js/1.b26be9da.chunk.js"
+  },
+  {
+    "revision": "56ec8a937be000867265",
+    "url": "/oderlive/todo.git/static/js/runtime~main.56ec8a93.js"
+  },
+  {
+    "revision": "5c51e67c7e1c9daf5e8678b2f6b5cbab",
+    "url": "/oderlive/todo.git/index.html"
+  }
+];
